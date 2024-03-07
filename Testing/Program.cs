341abfc0ad2385811dfd67ce67c02ab4a3cf1df4
Heyner Leiva <heynerleiva96@gmail.com>
2024-03-06 22:23:05 -0600

    Agregar archivos de proyecto.

---
+++ Testing/Program.cs
@@ -0,0 +1,26 @@
+﻿using InventSoft.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(inventEntities db = new inventEntities())
+            {
+                var productos = db.producto.ToList();
+
+                foreach(var p in productos)
+                {
+                    Console.WriteLine(p.nombre);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
